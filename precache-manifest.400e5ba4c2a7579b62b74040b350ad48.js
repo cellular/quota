793f3e3c764f05d006b4484d27c2b94931160f4a
Felix Gnass <fgnass@cellular.de>
2020-03-10 13:01:40 +0100

    Updates

---
+++ precache-manifest.400e5ba4c2a7579b62b74040b350ad48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3956f96788686aee44e0ba6a695cdf9a",
+    "url": "/quota/index.html"
+  },
+  {
+    "revision": "0e886a4e02d930317d56",
+    "url": "/quota/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "35ea107d31cc5223fed6",
+    "url": "/quota/static/js/2.93e45996.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/quota/static/js/2.93e45996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e886a4e02d930317d56",
+    "url": "/quota/static/js/main.ea38e801.chunk.js"
+  },
+  {
+    "revision": "a07a621e5559ceea91b9",
+    "url": "/quota/static/js/runtime-main.7d2ef4f6.js"
+  }
+]);
