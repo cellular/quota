3bfc1772a4cf51ae5cd7ec794c2b9bc8e75282d2
Felix Gnass <fgnass@cellular.de>
2020-03-10 13:00:12 +0100

    feat: fill indexeddb with random data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { clear, set } from "idb-keyval";
+import prettyBytes from "pretty-bytes";
+
+const chunks = [1000 * 1000 * 100, 1000 * 1000 * 10, 1000 * 1000, 1000];
 
 function App() {
+  const [size, setSize] = useState(0);
+  const [chunkType, setChunkType] = useState(0);
+  const [running, setRunning] = useState(false);
+  useEffect(() => {
+    const fill = async () => {
+      let chunk = chunks[chunkType];
+      try {
+        let array = new Uint8Array(chunk);
+        for (let i = 0; i < array.length; i++) {
+          array[i] = Math.floor(Math.random() * 255);
+        }
+        await set(size, array.buffer);
+        setSize(size + chunk);
+      } catch (err) {
+        if (chunkType < chunks.length - 1) {
+          setChunkType(chunkType + 1);
+        }
+      }
+    };
+    if (running) fill();
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>Filled {prettyBytes(size)}</div>
+      <button onClick={() => setRunning(true)}>Start</button>
+      <button onClick={() => setRunning(false)}>Stop</button>
+      <button
+        onClick={() => {
+          clear();
+          setSize(0);
+          setChunkType(0);
+        }}
+      >
+        Clear
+      </button>
+    </>
   );
 }
 
