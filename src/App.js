3a3b4774ccebecff8b1bfd925a1db2e76ee01f2e
Felix Gnass <fgnass@cellular.de>
2020-03-10 23:01:06 +0100

    feat: basic styling

--- src/App.js
+++ src/App.js
@@ -2,23 +2,61 @@ import React, { useEffect, useState } from "react";
 import { clear, set } from "idb-keyval";
 import prettyBytes from "pretty-bytes";
 
+function estimateStorage() {
+  if ("storage" in navigator && "estimate" in navigator.storage) {
+    // We've got the real thing! Return its response.
+    return navigator.storage.estimate();
+  }
+
+  if (
+    "webkitTemporaryStorage" in navigator &&
+    "queryUsageAndQuota" in navigator.webkitTemporaryStorage
+  ) {
+    // Return a promise-based wrapper that will follow the expected interface.
+    return new Promise(function(resolve, reject) {
+      navigator.webkitTemporaryStorage.queryUsageAndQuota(function(
+        usage,
+        quota
+      ) {
+        resolve({ usage: usage, quota: quota });
+      },
+      reject);
+    });
+  }
+
+  return Promise.reject();
+}
+
+const MAX_ENTROPY = 65536;
+function fillWithRandomBytes(array) {
+  let rnd = new Uint8Array(MAX_ENTROPY);
+  window.crypto.getRandomValues(rnd);
+  for (let i = 0; i < array.length - 1; i += rnd.length) {
+    let end = Math.min(rnd.length, array.length - i);
+    array.set(rnd.subarray(0, end), i);
+  }
+}
 const chunks = [1000 * 1000 * 100, 1000 * 1000 * 10, 1000 * 1000, 1000];
 
 function App() {
+  const [quota, setQuota] = useState(0);
   const [size, setSize] = useState(0);
   const [chunkType, setChunkType] = useState(0);
   const [running, setRunning] = useState(false);
+  useEffect(() => {
+    estimateStorage().then(({ quota }) => setQuota(quota));
+  }, []);
+
   useEffect(() => {
     const fill = async () => {
       let chunk = chunks[chunkType];
       try {
         let array = new Uint8Array(chunk);
-        for (let i = 0; i < array.length; i++) {
-          array[i] = Math.floor(Math.random() * 255);
-        }
+        fillWithRandomBytes(array);
         await set(size, array.buffer);
         setSize(size + chunk);
       } catch (err) {
+        console.log(err);
         if (chunkType < chunks.length - 1) {
           setChunkType(chunkType + 1);
         }
@@ -28,18 +66,28 @@ function App() {
   });
   return (
     <>
-      <div>Filled {prettyBytes(size)}</div>
-      <button onClick={() => setRunning(true)}>Start</button>
-      <button onClick={() => setRunning(false)}>Stop</button>
-      <button
-        onClick={() => {
-          clear();
-          setSize(0);
-          setChunkType(0);
-        }}
-      >
-        Clear
-      </button>
+      <h1>Quota Check</h1>
+      <p>
+        This app lets you test how much data can be stored in your browsers'
+        IndexedDB.
+      </p>
+      {quota && <p>The storage API reports a quota of {prettyBytes(quota)}.</p>}
+      <p>
+        Filled {prettyBytes(size)} (Chunk size {prettyBytes(chunks[chunkType])})
+      </p>
+      <p>
+        <button onClick={() => setRunning(true)}>Start</button>
+        <button onClick={() => setRunning(false)}>Stop</button>
+        <button
+          onClick={() => {
+            clear();
+            setSize(0);
+            setChunkType(0);
+          }}
+        >
+          Clear
+        </button>
+      </p>
     </>
   );
 }
